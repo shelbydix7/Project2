28351bd68e2c4a526cd020f2e275023db0f8f3c4
Zika Chea <jallsadou@gmail.com>
2018-09-23 01:38:48 -0500

    database setup

--- config/orm.js
+++ config/orm.js
@@ -3,8 +3,8 @@ var connection = require("../config/connection.js");
 
 // Object for all our SQL statement functions.
 var orm = {
-  all: function(input, cb) {
-    var queryString = "SELECT * FROM " + input + ";";
+  all: function( cb) {
+    var queryString = "SELECT * FROM user";
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
@@ -12,7 +12,21 @@ var orm = {
       cb(result);
     });
   },
+
+  create: function(cb, req) {
+    var queryString = "INSERT INTO user (name, score1, score2, score3, score4, score5) VALUES (?, ?, ?, ?, ?, ?)";
+    connection.query(queryString, [req.body.name, req.body.score1, req.body.score2, req.body.score3, req.body.score4, req.body.score5], function( err,result)
+    {
+    if (err) {
+      throw err;
+    }
+
+    cb(result);
+  
+  });
+}
+  
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model.
 module.exports = orm;
